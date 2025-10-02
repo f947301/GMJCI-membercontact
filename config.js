b588d8881b93a5056c88d24401ddaf8e47469757
xinbo <sam@crowndesign.com.tw>
2025-10-02 16:07:34 +0800

    no message

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // 💡 只需在這裡更新您的 GAS Web App URL
-const GAS_URL = "https://script.google.com/macros/s/AKfycbx04rNTTxPOHn6mAvQAL7MUSwJdR9-LKzA0-ng4Q7PjReGwjdKdzXmPWIINBTGAU5TIyA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzM1GF4hW2OCaGkMR9Kn3-z6HOI0Kw2UwQ5JhcMS0OMaqbN-5ui6JSIrdXe_74cvxcBcA/exec;
